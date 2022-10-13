1d6f7ee2629d46fab39c62771b53e435df6cb51d
rowendev <rowendevtw@gmail.com>
2022-10-13 12:11:05 +0800

    card, rwd

--- src/components/Taipei.jsx
+++ src/components/Taipei.jsx
@@ -8,7 +8,7 @@ function Taipei() {
   return (
     <div className="container">
       <svg
-        width="530"
+        width="500"
         height="550"
         className="taipei"
         xmlns="http://www.w3.org/2000/svg"
